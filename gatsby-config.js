2d8adce168ef9cd91edb451a8f3bebc921f36bb8
Jaya <82650452+real-jiakai@users.noreply.github.com>
2023-01-09 12:07:51 +0800

    change url

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'The Running Page Of Jiakai',
-    siteUrl: 'https://running.jiakai.top',
+    siteUrl: 'https://running.gujiakai.top',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
